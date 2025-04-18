41ed3d7c4c72fa5479d26647ab2f2de703b8cbbd
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 <moningisreedevi1234@gmail.com>
2025-04-18 16:32:54 +0530

    java program

---
+++ src/main/java/javaprograms/Ascendingorder.java
@@ -0,0 +1,15 @@
+package javaprograms;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Ascendingorder {
+
+	public static void main(String[] args) {
+		List<Integer> numberList=new ArrayList<Integer>(Arrays.asList(3,8,67,32,89,67,0,32,0,8));
+		numberList.stream().sorted().distinct().collect(Collectors.toList()).forEach(System.out::println);
+	}
+
+}
